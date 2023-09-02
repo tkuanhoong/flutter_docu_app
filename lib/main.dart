6451f1e7e74f9e40f561b8542b70f95f3d2a9382
tkuanhoong <tkuanhoong@gmail.com>
2023-09-02 09:40:51 +0800

    remove unwanted code

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_docu_app/ui/introduction/shopping_list.dart';
+import 'package:flutter_docu_app/ui/1.introduction/shopping_list.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        body: ShoppingList(
+      home: ShoppingList(
           products: [
             Product(name: 'Eggs'),
             Product(name: 'Flour'),
             Product(name: 'Chocolate chips'),
           ],
         ),
-      ),
     ),
   );
 }
