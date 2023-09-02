eb3c739d8b4047065f228eb12cbd7b7e93142f2a
tkuanhoong <tkuanhoong@gmail.com>
2023-09-02 08:53:11 +0800

    introduction

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_docu_app/ui/introduction/shopping_list.dart';
 
 void main() {
-  // The runApp() function takes the given Widget and makes it the root of the widget tree.
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: ShoppingList(
+          products: [
+            Product(name: 'Eggs'),
+            Product(name: 'Flour'),
+            Product(name: 'Chocolate chips'),
+          ],
+        ),
       ),
-    );
-  }
+    ),
+  );
 }
