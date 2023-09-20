a68adfd6923058910395054c9fe33b968ed78bf4
tkuanhoong <tkuanhoong@gmail.com>
2023-09-20 18:14:41 +0800

    drag a ui element

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_docu_app/theme/app_theme.dart';
-import 'package:flutter_docu_app/ui/4.interactivity/gestures/handle_tap.dart';
+import 'package:flutter_docu_app/ui/4.interactivity/gestures/drag_ui.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,15 +13,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HandleTap(),
+      home: const DragUi(),
       // Create an app theme
       theme: ThemeData(
-        useMaterial3: true,
+        // useMaterial3: true,
 
         // Default colors and brightness
-        colorScheme: AppTheme.colorScheme,
+        // colorScheme: AppTheme.colorScheme,
         // Default `TextTheme` for headlines, titles, bodies of text and more
-        textTheme: AppTheme.textTheme,
+        // textTheme: AppTheme.textTheme,
       ),
       // We may provide a dark theme here
       // darkTheme: ,
